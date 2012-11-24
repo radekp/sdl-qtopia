c458ba91a99e055e2d6d252c8b05a5ce401564de
Radek Polak <psonek2@seznam.cz>
2012-11-24 17:41:21 +0100

    mocSDL_QWin.cc - regenerated with latest moc

--- src/video/qtopia4/mocSDL_QWin.cc
+++ src/video/qtopia4/mocSDL_QWin.cc
@@ -1,8 +1,8 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'SDL_QWin.h'
 **
-** Created: Thu Oct 22 17:40:51 2009
-**      by: The Qt Meta Object Compiler version 59 (Qt 4.3.6)
+** Created: Sat Nov 24 16:39:48 2012
+**      by: The Qt Meta Object Compiler version 63 (Qt 4.8.3)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -10,21 +10,25 @@
 #include "SDL_QWin.h"
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'SDL_QWin.h' doesn't include <QObject>."
-#elif Q_MOC_OUTPUT_REVISION != 59
-#error "This file was generated using the moc from 4.3.6. It"
+#elif Q_MOC_OUTPUT_REVISION != 63
+#error "This file was generated using the moc from 4.8.3. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
 
+QT_BEGIN_MOC_NAMESPACE
 static const uint qt_meta_data_SDL_QWin[] = {
 
  // content:
-       1,       // revision
+       6,       // revision
        0,       // classname
        0,    0, // classinfo
        0,    0, // methods
        0,    0, // properties
        0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
 
        0        // eod
 };
@@ -33,21 +37,37 @@ static const char qt_meta_stringdata_SDL_QWin[] = {
     "SDL_QWin\0"
 };
 
+void SDL_QWin::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    Q_UNUSED(_o);
+    Q_UNUSED(_id);
+    Q_UNUSED(_c);
+    Q_UNUSED(_a);
+}
+
+const QMetaObjectExtraData SDL_QWin::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
+};
+
 const QMetaObject SDL_QWin::staticMetaObject = {
     { &QWidget::staticMetaObject, qt_meta_stringdata_SDL_QWin,
-      qt_meta_data_SDL_QWin, 0 }
+      qt_meta_data_SDL_QWin, &staticMetaObjectExtraData }
 };
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &SDL_QWin::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
 
 const QMetaObject *SDL_QWin::metaObject() const
 {
-    return &staticMetaObject;
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
 }
 
 void *SDL_QWin::qt_metacast(const char *_clname)
 {
     if (!_clname) return 0;
     if (!strcmp(_clname, qt_meta_stringdata_SDL_QWin))
-	return static_cast<void*>(const_cast< SDL_QWin*>(this));
+        return static_cast<void*>(const_cast< SDL_QWin*>(this));
     return QWidget::qt_metacast(_clname);
 }
 
@@ -58,3 +78,4 @@ int SDL_QWin::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
         return _id;
     return _id;
 }
+QT_END_MOC_NAMESPACE
